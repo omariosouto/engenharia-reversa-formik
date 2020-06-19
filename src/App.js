6ea00b0b0082d3665c03d7f271e6c6d225597efb
Mario Souto <soutomarios@gmail.com>
2020-06-19 16:57:30 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+
+function useFormik({
+  initialValues
+}) {  
+  const [values, setValues] = useState(initialValues); 
+
+  function handleChange(event) {
+    const fieldName = event.target.getAttribute('name');
+    const value = event.target.value;
+    setValues({
+      ...values,
+      [fieldName]: value,
+    })
+  }
+
+  return {
+    values,
+    handleChange
+  }
+}
 
 function App() {
+  const formik = useFormik({
+    initialValues: {
+      userEmail: 'email@email.com',
+      userPassword: '123456',
+    }
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <form onSubmit={(event) => {
+        event.preventDefault();
+        console.log(formik.values);
+      }}>
+        <div className="formField">
+          <label htmlFor="userEmail">
+            E-mail:
+          </label>
+          <input
+            type="text"
+            placeholder="email@example.com"
+            name="userEmail"
+            id="userEmail"
+            onChange={formik.handleChange}
+            value={formik.values.userEmail}
+          />
+          {/* <span class="formField__error">This field is required</span> */}
+        </div>
+
+        <div className="formField">
+          <label htmlFor="userPassword">
+            Password:
+          </label>
+          <input
+            type="password"
+            placeholder="Your secret password"
+            name="userPassword"
+            id="userPassword"
+            onChange={formik.handleChange}
+            value={formik.values.userPassword}
+          />
+          {/* <span class="formField__error">This field is required</span> */}
+        </div>
+
+        <button type="submit">
+          Login
+        </button>
+      </form>
   );
 }
 
