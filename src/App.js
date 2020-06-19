104ea73d9b5186bd92ea419324dffe598f6d326c
Mario Souto <soutomarios@gmail.com>
2020-06-19 17:11:35 -0300

    Minor Update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react';
 
-
 function useFormik({
-  initialValues
-}) {  
-  const [values, setValues] = useState(initialValues); 
+  initialValues,
+}) {
+  const [values, setValues] = useState(initialValues);
 
   function handleChange(event) {
     const fieldName = event.target.getAttribute('name');
-    const value = event.target.value;
+    const { value } = event.target;
     setValues({
       ...values,
       [fieldName]: value,
-    })
+    });
   }
 
   return {
     values,
-    handleChange
-  }
+    handleChange,
+  };
 }
 
 function App() {
@@ -26,48 +26,49 @@ function App() {
     initialValues: {
       userEmail: 'email@email.com',
       userPassword: '123456',
-    }
+    },
   });
 
   return (
-      <form onSubmit={(event) => {
-        event.preventDefault();
-        console.log(formik.values);
-      }}>
-        <div className="formField">
-          <label htmlFor="userEmail">
-            E-mail:
-          </label>
-          <input
-            type="text"
-            placeholder="email@example.com"
-            name="userEmail"
-            id="userEmail"
-            onChange={formik.handleChange}
-            value={formik.values.userEmail}
-          />
-          {/* <span class="formField__error">This field is required</span> */}
-        </div>
+    <form onSubmit={(event) => {
+      event.preventDefault();
+      console.log(formik.values);
+    }}
+    >
+      <div className="formField">
+        <label htmlFor="userEmail">
+          E-mail:
+        </label>
+        <input
+          type="text"
+          placeholder="email@example.com"
+          name="userEmail"
+          id="userEmail"
+          onChange={formik.handleChange}
+          value={formik.values.userEmail}
+        />
+        {/* <span class="formField__error">This field is required</span> */}
+      </div>
 
-        <div className="formField">
-          <label htmlFor="userPassword">
-            Password:
-          </label>
-          <input
-            type="password"
-            placeholder="Your secret password"
-            name="userPassword"
-            id="userPassword"
-            onChange={formik.handleChange}
-            value={formik.values.userPassword}
-          />
-          {/* <span class="formField__error">This field is required</span> */}
-        </div>
+      <div className="formField">
+        <label htmlFor="userPassword">
+          Password:
+        </label>
+        <input
+          type="password"
+          placeholder="Your secret password"
+          name="userPassword"
+          id="userPassword"
+          onChange={formik.handleChange}
+          value={formik.values.userPassword}
+        />
+        {/* <span class="formField__error">This field is required</span> */}
+      </div>
 
-        <button type="submit">
-          Login
-        </button>
-      </form>
+      <button type="submit">
+        Login
+      </button>
+    </form>
   );
 }
 
